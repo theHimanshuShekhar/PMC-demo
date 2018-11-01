75f584b758002d0d52b0d50e1589a39b9ae7ca0a
theHimanshuShekhar <hshekhar.dev@gmail.com>
2018-11-01 06:14:53 +0530

    added template for booking form

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,31 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbDate, NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css'],
-  styles: [`
-    .custom-day {
-      text-align: center;
-      padding: 0.185rem 0.25rem;
-      display: inline-block;
-      height: 2rem;
-      width: 2rem;
-    }
-    .custom-day.focused {
-      background-color: #e6e6e6;
-    }
-    .custom-day.range, .custom-day:hover {
-      background-color: rgb(2, 117, 216);
-      color: white;
-    }
-    .custom-day.faded {
-      background-color: rgba(2, 117, 216, 0.5);
-    }
-  `]
+  styleUrls: ['./booking.component.css']
 })
 
 
@@ -41,23 +21,10 @@ export class BookingComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    calendar: NgbCalendar,
     private dataService: DataService
   ) {
-    this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
   }
 
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -70,5 +37,10 @@ export class BookingComponent implements OnInit {
         this.router.navigate(['/']);
       }
     });
+  }
+
+  submitForm() {
+    console.log('Add booking request to database');
+    this.router.navigateByUrl('/account');
   }
 }
