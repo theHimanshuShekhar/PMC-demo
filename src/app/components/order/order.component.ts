2d945da06513f005f52c7f3df0aeecc41768e606
theHimanshuShekhar <hshekhar.dev@gmail.com>
2018-11-12 07:38:45 +0530

    enlarged modal

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -22,7 +22,6 @@ export class OrderComponent implements OnInit {
 
   @Input() booking;
   location;
-  closeResult;
   constructor(
     private dataService: DataService,
     private modalService: NgbModal
@@ -39,21 +38,7 @@ export class OrderComponent implements OnInit {
   }
 
   open(content) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+    this.modalService.open(content, { size: 'lg' });
   }
 
 }
